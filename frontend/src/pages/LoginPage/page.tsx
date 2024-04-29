21144906c1e6b33f2d35268b606e5ed73dd3a825
Bartosz Kaszuba <bartoszkaszuba1@gmail.com>
2024-04-29 12:52:37 +0200

    Progress on setting up login page

---
+++ frontend/src/pages/LoginPage/page.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+import styles from "./page.module.css";
+
+const LoginPage = () => {
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.card} />
+    </div>
+  );
+};
+
+export default LoginPage;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,1 +1,2 @@
-export { default as HomePage } from './HomePage/page';
+export { default as HomePage } from "./HomePage/page";
+export { default as LoginPage } from "./LoginPage/page";
